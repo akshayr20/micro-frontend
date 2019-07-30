bd5c69844c38101a12f0a77871ebd31ba65cb97a
akshayr20 <akshay.rajput5394@gmail.com>
2019-07-30 15:25:33 +0530

    saving WIP (create angular elements)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule, Injector } from '@angular/core';
+
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ButtonComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [ButtonComponent],
+  bootstrap:[AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const button = createCustomElement(ButtonComponent, { injector: this.injector });
+    customElements.define('app-button', button);
+  }
+}
