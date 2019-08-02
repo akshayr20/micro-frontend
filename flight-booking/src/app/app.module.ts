af92938a70fff9d46302d38ae7626a31086c4ed8
akshayr20 <akshay.rajput5394@gmail.com>
2019-08-02 10:53:46 +0530

    saving WIP fixing issue with loading multiple angular element on same page

--- flight-booking/src/app/app.module.ts
+++ flight-booking/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  entryComponents: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {}
+  ngDoBootstrap() {
+    const myCustomElement = createCustomElement(AppComponent, { injector: this.injector });
+    customElements.define('app-flight-booking', myCustomElement);
+  }
+}
